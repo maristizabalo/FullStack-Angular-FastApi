c1dbe0e6247750db4b6ad082fb37b092dcb86c3a
maristizabalo <84209839+maristizabalo@users.noreply.github.com>
2025-07-10 09:39:17 -0500

    Configurando navegacion a ruta principal luego de POST Ok

--- frontend/src/app/features/products/pages/product-form/product-form.component.ts
+++ frontend/src/app/features/products/pages/product-form/product-form.component.ts
@@ -71,7 +71,7 @@ export class ProductFormComponent implements OnInit {
           'Cerrar',
           { duration: 3000 }
         );
-        this.router.navigate(['/productos']);
+        this.router.navigate(['/']);
       },
       error: () => {
         this.loading = false;
